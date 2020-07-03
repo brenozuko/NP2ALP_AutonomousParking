56b7ab1bc9603db2bae52fd6796fb4ff78a78a9d
Breno Zukowski <firegameplaysadm@gmail.com>
2020-07-03 14:36:38 +0000

    Implementação relógio autônomo e bug fixes, modularização por andar, inserção de carros finalizada.

--- main.cpp
+++ main.cpp
@@ -29,21 +29,8 @@ void insere(int sthoras, int stminutos){
     minutos = stminutos;
 }
 
-
-void passaTempo(){
-    while(horas <= 24){   
-        minutos += 10;
-        if(minutos > 59){
-            minutos = 00;
-            horas++;
-            if(horas == 24){
-                horas = 00;
-                minutos = 00;
-                break;
-            }
-        }
-        cout << horas << ":" << minutos << endl;
-    }
+void mostrarHora(){
+    cout << "Hora atual: " << horas << ":" << minutos << endl; 
 }
 
 } Relogio;
@@ -54,6 +41,7 @@ void mostrarVeiculos(float preco,Veiculo Andar[], int tam);
 void inserirCarro(Veiculo car, Veiculo Andar[], int indice, int tam);
 void retirarCarro(Veiculo car, Veiculo Andar[], int indice, int tam);
 void calcularPreco(Veiculo car, int h, int m);
+void contaTempo(Relogio *Tempo);
 
 int main()
 {
@@ -72,9 +60,25 @@ int main()
   int opc, tipo,pos;
   int hora,minuto;
   int andar;
+  Relogio Expediente;
   Veiculo Automovel; //Variavel que vai assumir qualquer tipo de Veiculo
+
+ cout << "Horário de início do expediente [hh:mm]: ";
+ cin >> hora;
+ cin.get();
+ cin >> minuto;
+   
+
+
+  
   while(true)
   {
+      
+    Expediente.insere(hora,minuto);
+    contaTempo(&Expediente);
+    Expediente.mostrarHora();
+
+      
     cout << "1 - Estacionar um Veiculo, 2 - Retirar um Veiculo";
     cin >> opc;
 
@@ -85,11 +89,6 @@ int main()
       
       cout << "Qual a placa de seu carro: ";
       cin >> Automovel.placa;
-
-      cout << "Horário de entrada [hh:mm]: ";
-      cin >> hora;
-      cin.get();
-      cin >> minuto;
 
 
 
@@ -103,18 +102,26 @@ int main()
           //Escolha do andar especifico para cada tipo de Veiculo
           cout << "Em qual andar deseja Estacionar [1 ou 2]: ";
           cin >> andar;
-
-          cout << "Vagas Carros - Andar "<< andar << endl;
-          mostrarVeiculos(5, CarrosAndar1, 20);
-
-          cout << "Em qual posicao deseja adicionar: ";
-          cin >> pos;
-          
-          inserirCarro(Automovel, CarrosAndar1,pos, 20);
           while(andar != 1 && andar != 2)
           {
             cout << "Em qual andar deseja Estacionar [1 ou 2]: ";
             cin >> andar;
+          }
+          
+          cout << "Vagas Carros - Andar "<< andar << endl;
+          if(andar == 1)
+          {
+            mostrarVeiculos(5, CarrosAndar1, 20);
+            cout << "Em qual posicao deseja adicionar: ";
+            cin >> pos;
+            inserirCarro(Automovel, CarrosAndar1, pos, 20);
+          }
+          else if(andar == 2)
+          {
+            mostrarVeiculos(5, Andar2, 25);
+            cout << "Em qual posicao deseja adicionar: ";
+            cin >> pos;
+            inserirCarro(Automovel, Andar2,pos, 25);
           }
           
         break;
@@ -123,13 +130,6 @@ int main()
           Automovel.tipo = 2;
           Automovel.inserir(hora,minuto);
 
-          cout << "Vagas Camionetes Andar "<<andar << endl;
-          mostrarVeiculos(Automovel.preco, CarrosAndar1, 30);
-
-          cout << "Em qual posicao deseja adicionar o Veiculo: ";
-          cin >> pos;
-
-          inserirCarro(Automovel, CarrosAndar1, pos, 30);
           cout << "Em qual andar deseja Estacionar [1, 3 ou 5]: ";
           cin >> andar;
           while(andar != 1 && andar != 3 && andar != 5)
@@ -137,17 +137,35 @@ int main()
             cout << "Em qual andar deseja Estacionar [1, 3 ou 5]: ";
             cin >> andar;
           }
+          
+          cout << "Vagas Camionetes - Andar "<< andar << endl;
+          if(andar == 1)
+          {
+            mostrarVeiculos(7, CamionetesAndar1, 30);
+            cout << "Em qual posicao deseja adicionar: ";
+            cin >> pos;
+            inserirCarro(Automovel, CamionetesAndar1, pos, 30);
+          }
+          else if(andar == 3)
+          {
+            mostrarVeiculos(7, Andar3, 10);
+            cout << "Em qual posicao deseja adicionar: ";
+            cin >> pos;
+            inserirCarro(Automovel, Andar3, pos, 10);
+          }
+          else if(andar == 5)
+          {
+            mostrarVeiculos(7, Andar5, 20);
+            cout << "Em qual posicao deseja adicionar: ";
+            cin >> pos;
+            inserirCarro(Automovel, Andar5, pos, 20);
+          }
 
         break;
         case 3:
-        cout 
           Automovel.preco = 10;
           Automovel.tipo = 3;
           Automovel.inserir(hora,minuto);
-          cout << Automovel.hora << ":" << Automovel.minuto << endl;
-
-
-
           cout << "Em qual andar deseja Estacionar [4]: ";
           cin >> andar;
           while(andar != 4)
@@ -155,7 +173,16 @@ int main()
             cout << "Em qual andar deseja Estacionar [4]: ";
             cin >> andar;
           }
-
+          
+          cout << "Vagas Super-Esportivos - Andar "<< andar << endl;
+          if(andar == 4)
+          {
+            mostrarVeiculos(5, CarrosAndar1, 20);
+            cout << "Em qual posicao deseja adicionar: ";
+            cin >> pos;
+            inserirCarro(Automovel, Andar4,pos,15);
+          }
+          
         break;
       }
     }
@@ -215,4 +242,17 @@ void mostrarVeiculos(float preco,Veiculo Andar[], int tam)
     }
   }
 }
+
+void contaTempo(Relogio *Tempo) {
+    Tempo->minutos += 10;
+        if(Tempo->minutos > 59){
+            Tempo->minutos = 00;
+            Tempo->horas++;
+            if(Tempo->horas == 24){
+               Tempo->horas = 00;
+               Tempo->minutos = 00;
+            }
+        }
+}
+
 
